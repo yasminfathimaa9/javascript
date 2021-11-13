289e58507b6d9efc899d29680bb07fecbc4b1461
yasminfathimaa9 <yasminfathimaa9@gmail.com>
2021-11-13 17:04:58 +0530

    javascrpt

---
+++ index.js
@@ -0,0 +1,275 @@
+var jsondata={
+    status : "ok",
+    totalResults : 9551,
+    articles : [
+   {
+   source: {
+    id: null,
+    name: "CleanTechnica"
+   },
+    author: "Guest Contributor",
+    title: "Can Tesla Become the Apple of Automakers?",
+    description: "Originally posted on EVANNEX. By Charles Morris As media pundits (including myself) never tire of telling us, Tesla and Apple have a number of things in common — both grew very quickly on the strength of a revolutionary invention, and each has created an ecos…",
+    url: "https://cleantechnica.com/2021/11/12/can-tesla-become-the-apple-of-automakers/",
+    urlToImage: "https://cleantechnica.com/files/2021/04/Apple-Car-Tesla-Model-3-CleanTechnica-Hood-Watermark.png",
+    publishedAt: "2021-11-13T04:48:35Z",
+    content: "Originally posted on EVANNEX.By Charles Morris\r\nAs media pundits (including myself) never tire of telling us, Tesla and Apple have a number of things in common both grew very quickly on the strength … [+5424 chars]"
+   },
+   {
+   source : {
+    id: null,
+   name: "NDTV News"
+   },
+   author: "Reuters",
+   title: "Elon Musk Sells a Total of $6.9 Billion Worth of Tesla Shares This Week",
+   description: "Tesla CEO Elon Musk offloaded a combined $6.9 billion (roughly Rs. 51,300 crore) worth of shares in the electric car company this week, taking advantage of a meteoric rally that vaulted the firm's value to over $1 trillion (roughly Rs. 74,34,725 crore).",
+   url: "https://gadgets.ndtv.com/transportation/news/elon-musk-tesla-share-sell-usd-6-9-million-twitter-poll-2609185",
+   urlToImage: "https://i.gadgets360cdn.com/large/elon_musk_bloomberg_1633671660571.jpg",
+   publishedAt: "2021-11-13T04:24:12Z",
+   "content": "Tesla CEO Elon Musk offloaded a combined $6.9 billion worth of shares in the electric car company this week, taking advantage of a meteoric rally that vaulted the firm's value to over $1 trillion.\r\nT… [+2786 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "CleanTechnica"
+   },
+   author: "Johnna Crider",
+   title: "Tesla China Offered 11/11 Enhanced Autopilot Free Experience In Honor Of China Singles’ Day",
+   description: "In the U.S., November 11 is known for being a day set aside to honor veterans. Globally, this date also has significance in various countries. In China, Double 11 is known as Singles’ Day or Bachelors’ Day. It’s an unofficial holiday and shopping season celeb…",
+   url: "https://cleantechnica.com/2021/11/12/tesla-china-offered-11-11-enhanced-autopilot-free-experience-in-honor-of-china-singles-day/",
+   urlToImage: "https://cleantechnica.com/files/2021/11/Screen-Shot-2021-11-12-at-10.44.08-PM.png",
+   publishedAt: "2021-11-13T04:18:43Z",
+   content: "In the U.S., November 11 is known for being a day set aside to honor veterans. Globally, this date also has significance in various countries. In China, Double 11 is known as Singles’ Day or Bachelor… [+1759 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "Fox Business"
+   },
+   author: "Andrew Mark Miller",
+   title: "Elon Musk reveals 'worst person' he's ever worked with who 'came close to killing Tesla'",
+   description: "Tesla co-founder and CEO Elon Musk slammed the company's co-founder Martin Eberhard on Twitter Friday calling him the \"worst person\" he's ever worked with and accusing him of almost \"killing Tesla.\"",
+   url: "https://www.foxbusiness.com/politics/elon-musk-reveals-worst-person-hes-ever-worked-with-who-came-close-to-killing-tesla",
+   urlToImage: "https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2021/11/0/0/Elon-Musk.jpg?ve=1&tl=1",
+   publishedAt: "2021-11-13T03:35:53Z",
+   content: "Tesla\r\n co-founder and CEO Elon Musk\r\n slammed the company’s co-founder Martin Eberhard on Twitter Friday calling him the \"worst person\" he’s ever worked with and accusing him of almost \"killing Tesl… [+2602 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "Moneycontrol"
+   },
+   author: null,
+   title: "Wall Street ends higher with boost from big tech",
+   description: "The Dow Jones Industrial Average rose 179.08 points, or 0.5%, to 36,100.31. The S&P 500 gained 33.58 points, or 0.72%, at 4,682.85 and the Nasdaq Composite added 156.68 points, or 1%, at 15,860.96.",
+   url: "https://www.moneycontrol.com/news/business/markets/wall-street-ends-higher-with-boost-from-big-tech-7715031.html",
+   "urlToImage": "https://images.moneycontrol.com/static-mcnews/2020/10/NYSEwall-street-770x433.jpg",
+   "publishedAt": "2021-11-13T03:07:17Z",
+   "content": "Wall Street stocks closed higher on Friday, with market-leading growth shares kick-starting indexes climb as investors looked past disappointing U.S. economic data.\r\nDespite their advances, all three… [+2916 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "Livemint"
+   },
+   author: "Livemint",
+   title: "Elon Musk sold another $1.2 billion of Tesla stock on Friday",
+   description: "Elon Musk sold 1.2 million shares held by his trust on Nov. 12 for more than $1.2 billion",
+   url: "https://www.livemint.com/companies/news/elon-musk-sold-another-1-2-billion-of-tesla-stock-on-friday-11636772202607.html",
+   urlToImage: "https://images.livemint.com/img/2021/11/13/600x338/Philanthropy-Billionaries-Tax-1_1635478277506_1636772562114.jpg",
+   publishedAt: "2021-11-13T03:06:01Z",
+   content: "Congratulations!\r\nYou are now subscribed to our newsletters. In case you cant find any email from our side, please check the spam folder."
+   },
+   {
+   source: {
+   id: null,
+   name: "Tech Times"
+   },
+   author: "RJ Pierce",
+   title: "AMC Theaters Are Now Letting People Buy Movie Tickets With Crypto",
+   description: "AMC Theaters has officially started accepting crypto payments for online ticket sales.",
+   url: "https://www.techtimes.com/articles/267951/20211112/amc-theaters-crypto.htm",
+   urlToImage: "https://1734811051.rsc.cdn77.org/data/images/full/396066/dogecoin.jpg",
+   publishedAt: "2021-11-13T03:00:23Z",
+   content: "AMC Theaters has now officially launched crypto payments for buying movie tickets online.\r\nAccording to The Verge, AMC is now offering movie tickets for cryptocurrency payments. They're accepting Bit… [+3502 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "CNA"
+   },
+   author: null,
+   title: "Tesla's Musk sold another US$1.2 billion of stock on Nov. 12-filing",
+   description: "Tesla Inc Chief Executive Elon Musk sold 1.2 million shares held by his trust on Nov. 12 for more than US$1.2 billion, U.S. securities filings showed on Friday, adding to the multi-billion dollar list of sales reported this week. (Reporting by Peter Henderson…",
+   url: "https://www.channelnewsasia.com/business/teslas-musk-sold-another-us12-billion-stock-nov-12-filing-2311221",
+   urlToImage: "https://onecms-res.cloudinary.com/image/upload/s--t3VXN1aX--/fl_relative%2Cg_south_east%2Cl_one-cms:core:watermark:reuters%2Cw_0.1/f_auto%2Cq_auto/c_fill%2Cg_auto%2Ch_676%2Cw_1200/v1/one-cms/core/2021-11-13t025200z_2_lynxmpehac016_rtroptp_3_tesla-berlin-opening.jpg?itok=kHtEGR_I",
+   publishedAt: "2021-11-13T02:54:33Z",
+   content: "We know it's a hassle to switch browsers but we want your experience with CNA to be fast, secure and the best it can possibly be.\r\nTo continue, upgrade to a supported browser or, for the finest exper… [+30 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "Slashdot.org"
+   },
+   author: "feedfeeder",
+   title: "Tesla's 'Full Self-Driving' Beta Appears to Have Caused Its First Major Crash - Gizmodo",
+   description: "Tesla's 'Full Self-Driving' Beta Appears to Have Caused Its First Major CrashGizmodo Tesla vehicle in ‘Full Self-Driving’ beta mode ‘severely damaged’ after crash in CaliforniaThe Verge The First Crash Of A Tesla Using FSD Beta May Have HappenedJalopnik Tesla…",
+   url: "https://slashdot.org/firehose.pl?op=view&amp;id=155086665",
+   urlToImage: null,
+   publishedAt: "2021-11-13T02:53:16Z",
+   content: "The Fine Print: The following comments are owned by whoever posted them. We are not responsible for them in any way."
+   },
+   {
+   source: {
+   id: null,
+   name: "Monevator.com"
+   },
+   author: "The Investor",
+   title: "Weekend reading: Move on up",
+   description: "Why inflation is starting to look sticky. Plus the rest of the week's good reads…\nThe post Weekend reading: Move on up appeared first on Monevator.",
+   url: "https://monevator.com/weekend-reading-move-on-up/",
+   urlToImage: "https://monevator-monevator.netdna-ssl.com/wp-content/uploads/2021/11/Covid-new-waves-Europe.jpg",
+   publishedAt: "2021-11-13T02:02:40Z",
+   content: "What caught my eye this week.\r\nThe markets are throwing in the towel on the notion of ‘transitory’ inflation, or at least transitory US inflation. The US consumer price index just jumped 6.2% the mos… [+15192 chars]"
+   },
+   {
+   source: {
+   id: "independent",
+   name: "Independent"
+   },
+   author: "Via AP news wire",
+   title: "Tesla driver's complaint being looked into by US regulators",
+   description: "U.S. auto safety regulators are looking into a complaint from a Tesla driver that the company’s “Full Self-Driving” software caused a crash",
+   url: "https://www.independent.co.uk/news/tesla-detroit-model-y-joe-biden-driving-b1956994.html",
+   urlToImage: "",
+   publishedAt: "2021-11-13T01:51:40Z",
+   content: "U.S. auto safety regulators are looking into a complaint from a Tesla driver that the companys Full Self-Driving software caused a crash.\r\nThe driver was beta testing the Full Self-Driving software, … [+3386 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "Seeking Alpha"
+   },
+   author: "SA Transcripts",
+   title: "Sunworks' (SUNW) CEO Gaylon Morris on Q3 2021 Results - Earnings Call Transcript",
+   description: "Sunworks, Inc. (NASDAQ:SUNW) Q3 2021 Earnings Conference Call November 11, 2021 5:00 PM ETCompany ParticipantsJeff Stanlis – FNK Investor RelationsGaylon Morris – Chief Executive...",
+   url: "https://seekingalpha.com/article/4469111-sunworks-sunw-ceo-gaylon-morris-on-q3-2021-results-earnings-call-transcript",
+   urlToImage: "https://static.seekingalpha.com/assets/og_image_1200-29b2bfe1a595477db6826bd2126c63ac2091efb7ec76347a8e7f81ba17e3de6c.png",
+   publishedAt: "2021-11-13T01:43:05Z",
+   content: "Sunworks, Inc. (NASDAQ:SUNW) Q3 2021 Earnings Conference Call November 11, 2021 5:00 PM ET\r\n\nCompany Participants\r\n\nJeff Stanlis – FNK Investor Relations\r\n\nGaylon Morris – Chief Executive Officer\r\n\nP… [+22477 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "seattlepi.com"
+   },
+   author: null,
+   title: "Tesla driver's complaint being looked into by US regulators",
+   description: "DETROIT (AP) — U.S. auto safety regulators are looking into a complaint from a Tesla driver that the company’s “Full Self-Driving” software caused a crash.\nThe driver was beta testing the “Full Self-Driving” software, and the Tesla SUV went into the wrong lan…",
+   url: "https://www.seattlepi.com/news/article/Tesla-driver-s-complaint-being-looked-into-by-US-16617122.php",
+   urlToImage: "https://www.seattlepi.com/img/pages/article/opengraph_default.png",
+   publishedAt: "2021-11-13T01:37:44Z",
+   content: "DETROIT (AP) U.S. auto safety regulators are looking into a complaint from a Tesla driver that the companys Full Self-Driving software caused a crash.The driver was beta testing the Full Self-Driving… [+3387 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "Livemint"
+   },
+   author: "AP",
+   title: "Tesla driver's complaint being looked into by US regulators",
+   description: "The driver was beta testing the ‘Full Self-Driving’ software, and the Tesla SUV went into the wrong lane",
+   url: "https://www.livemint.com/companies/news/tesla-driver-s-complaint-being-looked-into-by-us-regulators-11636767207624.html",
+   urlToImage: "https://images.livemint.com/img/2021/11/13/600x338/TESLA-MUSK-STAKE-OPTIONS-0_1636767301466_1636767307652.JPG",
+   publishedAt: "2021-11-13T01:37:23Z",
+   content: "U.S. auto safety regulators are looking into a complaint from a Tesla driver that the companys Full Self-Driving\" software caused a crash.\r\nThe driver was beta testing the Full Self-Driving\" software… [+3599 chars]"
+   },
+   {
+   source: {
+   id: "abc-news",
+   name: "ABC News"
+   },
+   author: "The Associated Press",
+   title: "Tesla driver's complaint being looked into by US regulators",
+   description: "U.S. auto safety regulators are looking into a complaint from a Tesla driver that the company’s “Full Self-Driving” software caused a crash",
+   url: "https://abcnews.go.com/Business/wireStory/tesla-drivers-complaint-looked-us-regulators-81141802",
+   urlToImage: "https://s.abcnews.com/images/Technology/WireAP_d84a315d355343b382a9f86795a20586_16x9_992.jpg",
+   publishedAt: "2021-11-13T01:30:15Z",
+   content: "DETROIT -- U.S. auto safety regulators are looking into a complaint from a Tesla driver that the companys Full Self-Driving software caused a crash.\r\nThe driver was beta testing the Full Self-Driving… [+3389 chars]"
+   },
+   {
+   source: {
+   id: "abc-news",
+   name: "ABC News"
+   },
+   author: "The Associated Press",
+   title: "Tesla driver's complaint being looked into by US regulators",
+   description: "U.S. auto safety regulators are looking into a complaint from a Tesla driver that the company’s “Full Self-Driving” software caused a crash",
+   url: "https://abcnews.go.com/Technology/wireStory/tesla-drivers-complaint-looked-us-regulators-81141801",
+   urlToImage: "null",
+   publishedAt: "2021-11-13T01:30:01Z",
+   content: "DETROIT -- U.S. auto safety regulators are looking into a complaint from a Tesla driver that the companys Full Self-Driving software caused a crash.\r\nThe driver was beta testing the Full Self-Driving… [+3389 chars]"
+   },
+   {
+   source: {
+   id: "associated-press",
+   name: "Associated Press"
+   },
+   author: "",
+   title: "Tesla driver's complaint being looked into by US regulators",
+   description: "DETROIT (AP) — U.S. auto safety regulators are looking into a complaint from a Tesla driver that the company’s “Full Self-Driving” software caused a crash.",
+   url: "https://apnews.com/9c3d3c75d9c668dbb1a8cdbbe883ff88",
+   urlToImage: "https://bloximages.newyork1.vip.townnews.com/roanoke.com/content/tncms/custom/image/00f1ee32-f2d0-11e6-9926-f7249dbfe53a.jpg",
+   publishedAt: "2021-11-13T01:29:39Z",
+   content: "DETROIT (AP) U.S. auto safety regulators are looking into a complaint from a Tesla driver that the companys Full Self-Driving software caused a crash.\r\nThe driver was beta testing the Full Self-Drivi… [+1243 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "24/7 Wall St."
+   },
+   author: "247chrislange",
+   title: "Cathie Wood’s ARK Invest Sells for 11/12",
+   description: "Here's a look at what the ARK Invest exchange-traded funds sold on November 12, 2021.",
+   url: "https://247wallst.com/investing/2021/11/12/cathie-woods-ark-invest-sells-for-11-12/",
+   urlToImage: "https://247wallst.com/wp-content/uploads/2018/03/finance-background.jpg",
+   publishedAt: "2021-11-13T01:28:13Z",
+   content: "The S&amp;P 500, Dow Jones, and Nasdaq bounced back yet again on Friday after a couple down days in the middle of the week. ARK Invest funds rallied as well with one small exception. ARKF performed t… [+4465 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "24/7 Wall St."
+   },
+   author: "247chrislange",
+   title: "Cathie Wood’s ARK Invest Buys for 11/12",
+   description: "Here's a look at what the ARK Invest exchange-traded funds bought on November 12, 2021.",
+   url: "https://247wallst.com/investing/2021/11/12/cathie-woods-ark-invest-buys-for-11-12/",
+   urlToImage: "https://247wallst.com/wp-content/uploads/2018/12/imageForEntry1-6.jpg",
+   publishedAt: "2021-11-13T01:25:27Z",
+   content: "Markets continued to bounce back on Friday but concerns about inflation are still persisting. ARK Funds rallied with the markets as well with one small exception. ARKF performed the best out of the g… [+4383 chars]"
+   },
+   {
+   source: {
+   id: null,
+   name: "The Information"
+   },
+   author: "Martin Peers",
+   title: "The Flaws in CEO Management-By-Twitter",
+   description: "Elon Musk’s decision to poll Twitter on whether he should sell stock in Tesla triggered endless news coverage this week, as reporters and commentators dissected what he was really up to. The smartest take—in this Wall Street Journal commentary—was that Musk w…",
+   url: "https://www.theinformation.com/articles/the-flaws-in-ceo-management-by-twitter",
+   urlToImage: "https://tii.imgix.net/production/articles/6584/f31c7ffd-9388-4c3b-86a7-b13e76e925f1-enKhJE.jpg?w=1200&fm=jpg&auto=compress",
+   publishedAt: "2021-11-13T01:00:58Z",
+   content: "Elon Musk’s decision to poll Twitter on whether he should sell stock in Tesla triggered endless news coverage this week, as reporters and commentators dissected what he was really up to. The smartest… [+915 chars]"
+   }
+   ]
+   }
+
+   for (var i = 0; i < jsondata.articles.length ;i++) {
+       console.log(jsondata.articles[i].source.name);
+       console.log(jsondata.articles[i].title);
+      
+   }
+   
+   //output:displays names and titles
+
